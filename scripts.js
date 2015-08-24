cccaa097d640a6bf7fdb8b07e5f6013fbc089e6a
aliciasecord <alicia.m.secord@gmail.com>
2015-08-23 20:37:21 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,189 @@
 $(document).ready(function(){
+   
+   $("div.county_info").hide();
+   
+   $("area.alcona").click(){
+      $("div.alcona").show();
+      $("div.allegan").hide();
+      $("div.alger").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+    });
+   
+   $("area.alger").click(){
+      $("div.alger").show();
+      $("div.alcona").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+    });
+   
+   $("area.allegan").click(function(){
+      $("div.allegan").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+    });
+    
+   $("area.alpena").click(function(){
+      $("div.alpena").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+   });
+   
+   $("area.antrim").click(function(){
+      $("div.antrim").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+   });
+   
+   $("area.arenac").click(function(){
+      $("div.arenac").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+   });
+   
+   $("area.baraga").click(function(){
+      $("div.baraga").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.barry").hide();
+      $("div.bay").hide();
+   });
+   
+   $("area.barry").click(function(){
+      $("div.barry").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.bay").hide();
+   });
+   
+   $("area.bay").click(function(){
+      $("div.bay").show();
+      $("div.alcona").hide();
+      $("div.alger").hide();
+      $("div.allegan").hide();
+      $("div.alpena").hide();
+      $("div.antrim").hide();
+      $("div.arenac").hide();
+      $("div.baraga").hide();
+      $("div.barry").hide();
+   });
+}
 
-   // jQuery methods go here...
+      <area name="benzie" shape="poly" coords="" href="#" alt="Benzie">
+      <area name="berrien" shape="poly" coords="" href="#" alt="Berrien">
+      <area name="branch" shape="poly" coords="" href="#" alt="Branch">
+      <area name="calhoun" shape="poly" coords="" href="#" alt="Calhoun">
+      <area name="cass" shape="poly" coords="" href="#" alt="Cass">
+      <area name="charlevoix" shape="poly" coords="" href="#" alt="Charlevoix">
+      <area name="cheboygan" shape="poly" coords="" href="#" alt="Cheboygan">
+      <area name="chippewa" shape="poly" coords="" href="#" alt="Chippewa">
+      <area name="clare" shape="poly" coords="" href="#" alt="Clare">
+      <area name="clinton" shape="poly" coords="" href="#" alt="Clinton">
+      <area name="crawford" shape="poly" coords="" href="#" alt="Crawford">
+      <area name="delta" shape="poly" coords="" href="#" alt="Delta">
+      <area name="dickinson" shape="poly" coords="" href="#" alt="Dickinson">
+      <area name="eaton" shape="poly" coords="" href="#" alt="Eaton">
+      <area name="emmet" shape="poly" coords="" href="#" alt="Emmet">
+      <area name="genesee" shape="poly" coords="" href="#" alt="Genesee">
+      <area name="gladwin" shape="poly" coords="" href="#" alt="Gladwin">
+      <area name="gogebic" shape="poly" coords="" href="#" alt="Gogebic">
+      <area name="grand_traverse" shape="poly" coords="" href="#" alt="Grand Traverse">
+      <area name="gratiot" shape="poly" coords="" href="#" alt="Gratiot">
+      <area name="hillsdale" shape="poly" coords="" href="#" alt="Hillsdale">
+      <area name="houghton" shape="poly" coords="" href="#" alt="Houghton">
+      <area name="huron" shape="poly" coords="" href="#" alt="Huron">
+      <area name="ingham" shape="poly" coords="" href="#" alt="Ingham">
+      <area name="ionia" shape="poly" coords="" href="#" alt="Ionia">
+      <area name="iosco" shape="poly" coords="" href="#" alt="Iosco">
+      <area name="iron" shape="poly" coords="" href="#" alt="Iron">
+      <area name="isabella" shape="poly" coords="" href="#" alt="Isabella">
+      <area name="jackson" shape="poly" coords="" href="#" alt="Jackson">
+      <area name="kalamazoo" shape="poly" coords="" href="#" alt="Kalamazoo">
+      <area name="kalkaska" shape="poly" coords="" href="#" alt="Kalkaska">
+      <area name="kent" shape="poly" coords="" href="#" alt="Kent">
+      <area name="keweenaw" shape="poly" coords="" href="#" alt="Keweenaw">
+      <area name="lake" shape="poly" coords="" href="#" alt="Lake">
+      <area name="lapeer" shape="poly" coords="" href="#" alt="Lapeer">
+      <area name="leelanau" shape="poly" coords="" href="#" alt="Leelanau">
+      <area name="lenawee" shape="poly" coords="" href="#" alt="Lenawee">
+      <area name="livingston" shape="poly" coords="" href="#" alt="Livingston">
+      <area name="luce" shape="poly" coords="" href="#" alt="Luce">
+      <area name="mackinac" shape="poly" coords="" href="#" alt="Mackinac">
+      <area name="macomb" shape="poly" coords="" href="#" alt="Macomb">
+      <area name="manistee" shape="poly" coords="" href="#" alt="Manistee">
+      <area name="marquette" shape="poly" coords="" href="#" alt="Marquette">
+      <area name="mason" shape="poly" coords="" href="#" alt="Mason">
+      <area name="mecosta" shape="poly" coords="" href="#" alt="Mecosta">
+      <area name="menominee" shape="poly" coords="" href="#" alt="Menominee">
+      <area name="midland" shape="poly" coords="" href="#" alt="Midland">
+      <area name="missaukee" shape="poly" coords="" href="#" alt="Missaukee">
+      <area name="" shape="poly" coords="" href="#" alt="Monroe">
+      <area name="" shape="poly" coords="" href="#" alt="Montcalm">
+      <area name="" shape="poly" coords="" href="#" alt="Montmorency">
+      <area name="" shape="poly" coords="" href="#" alt="Muskegon">
+      <area name="" shape="poly" coords="" href="#" alt="Newaygo">
+      <area name="" shape="poly" coords="" href="#" alt="Oakland">
+      <area name="" shape="poly" coords="" href="#" alt="Oceana">
+      <area name="" shape="poly" coords="" href="#" alt="Ogemaw">
+      <area name="" shape="poly" coords="" href="#" alt="Ontonagon">
+      <area name="" shape="poly" coords="" href="#" alt="Osceola">
+      <area name="" shape="poly" coords="" href="#" alt="Oscoda">
+      <area name="" shape="poly" coords="" href="#" alt="Otsego">
+      <area name="" shape="poly" coords="" href="#" alt="Ottawa">
+      <area name="" shape="poly" coords="" href="#" alt="Presque Isle">
+      <area name="" shape="poly" coords="" href="#" alt="Roscommon">
+      <area name="" shape="poly" coords="" href="#" alt="Saginaw">
+      <area name="" shape="poly" coords="" href="#" alt="St. Clair">
+      <area name="" shape="poly" coords="" href="#" alt="St. Joseph">
+      <area name="" shape="poly" coords="" href="#" alt="Sanilac">
+      <area name="" shape="poly" coords="" href="#" alt="Schoolcraft">
+      <area name="" shape="poly" coords="" href="#" alt="Shiawassee">
+      <area name="" shape="poly" coords="" href="#" alt="Tuscola">
+      <area name="" shape="poly" coords="" href="#" alt="Van Buren">
+      <area name="" shape="poly" coords="" href="#" alt="Washtenaw">
+      <area name="" shape="poly" coords="" href="#" alt="Wayne">
+      <area name="" shape="poly" coords="" href="#" alt="Wexford">
 
 });
