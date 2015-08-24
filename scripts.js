8741c5ed705fe39638610ce9d8f16405da719bc2
aliciasecord <alicia.m.secord@gmail.com>
2015-08-23 22:55:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,142 +1,193 @@
 $(document).ready(function(){
-   
+
+/* Hide information about specific counties when nothing has been clicked. */   
    $("div.county_info").hide();
-   
+
+/* Show instructions div when nothing has been clicked yet */
+   $("div.instructions").show();
+
+/* Show information about each county when the county name or map area is clicked. */      
    $("area.alcona").click(){
+      $("div.county_info").hide();
       $("div.alcona").show();
-      $("div.allegan").hide();
-      $("div.alger").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
     });
    
    $("area.alger").click(){
+      $("div.county_info").hide();   
       $("div.alger").show();
-      $("div.alcona").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
     });
    
    $("area.allegan").click(function(){
+      $("div.county_info").hide();   
       $("div.allegan").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
     });
     
    $("area.alpena").click(function(){
+      $("div.county_info").hide();   
       $("div.alpena").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
    });
    
    $("area.antrim").click(function(){
+      $("div.county_info").hide();   
       $("div.antrim").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
    });
    
    $("area.arenac").click(function(){
+      $("div.county_info").hide();   
       $("div.arenac").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
    });
    
    $("area.baraga").click(function(){
+      $("div.county_info").hide();   
       $("div.baraga").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.barry").hide();
-      $("div.bay").hide();
    });
    
    $("area.barry").click(function(){
+      $("div.county_info").hide();
       $("div.barry").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.bay").hide();
    });
    
    $("area.bay").click(function(){
+      $("div.county_info").hide();
       $("div.bay").show();
-      $("div.alcona").hide();
-      $("div.alger").hide();
-      $("div.allegan").hide();
-      $("div.alpena").hide();
-      $("div.antrim").hide();
-      $("div.arenac").hide();
-      $("div.baraga").hide();
-      $("div.barry").hide();
+   });
+   
+   $("area.benzie").click(function(){
+      $("div.county_info").hide();
+      $("div.benzie").show();
+   });
+   
+   $("area.berrien").click(function(){
+      $("div.county_info").hide();
+      $("div.berrien").show();
+   });
+   
+   $("area.branch").click(function(){
+      $("div.county_info").hide();
+      $("div.branch").show();
+   });
+   
+   $("area.calhoun").click(function(){
+      $("div.county_info").hide();
+      $("div.calhoun").show();
+   });
+   
+   $("area.cass").click(function(){
+      $("div.county_info").hide();
+      $("div.cass").show();
+   });
+   
+   $("area.charlevoix").click(function(){
+      $("div.county_info").hide();
+      $("div.charlevoix").show();
+   });
+   
+   $("area.cheboygan").click(function(){
+      $("div.county_info").hide();
+      $("div.cheboygan").show();
+   });
+   
+   $("area.chippewa").click(function(){
+      $("div.county_info").hide();
+      $("div.chippewa").show();
+   });
+   
+   $("area.clare").click(function(){
+      $("div.county_info").hide();
+      $("div.clare").show();
+   });
+      
+   $("area.clinton").click(){
+      $("div.county_info").hide();
+      $("div.clinton").show();
+    });
+   
+   $("area.crawford").click(){
+      $("div.county_info").hide();   
+      $("div.crawford").show();
+    });
+   
+   $("area.delta").click(function(){
+      $("div.county_info").hide();   
+      $("div.delta").show();
+    });
+    
+   $("area.dickinson").click(function(){
+      $("div.county_info").hide();   
+      $("div.dickinson").show();
+   });
+   
+   $("area.eaton").click(function(){
+      $("div.county_info").hide();   
+      $("div.eaton").show();
+   });
+   
+   $("area.emmet").click(function(){
+      $("div.county_info").hide();   
+      $("div.emmet").show();
+   });
+   
+   $("area.genesee").click(function(){
+      $("div.county_info").hide();   
+      $("div.genesee").show();
+   });
+   
+   $("area.gladwin").click(function(){
+      $("div.county_info").hide();
+      $("div.gladwin").show();
+   });
+   
+   $("area.gogebic").click(function(){
+      $("div.county_info").hide();
+      $("div.gogebic").show();
+   });
+   
+   $("area.grand_traverse").click(function(){
+      $("div.county_info").hide();
+      $("div.grand_traverse").show();
+   });
+   
+   $("area.gratiot").click(function(){
+      $("div.county_info").hide();
+      $("div.gratiot").show();
+   });
+   
+   $("area.hillsdale").click(function(){
+      $("div.county_info").hide();
+      $("div.hillsdale").show();
+   });
+   
+   $("area.houghton").click(function(){
+      $("div.county_info").hide();
+      $("div.houghton").show();
+   });
+   
+   $("area.huron").click(function(){
+      $("div.county_info").hide();
+      $("div.huron").show();
+   });
+   
+   $("area.ingham").click(function(){
+      $("div.county_info").hide();
+      $("div.ingham").show();
+   });
+   
+   $("area.ionia").click(function(){
+      $("div.county_info").hide();
+      $("div.ionia").show();
+   });
+   
+   $("area.iosco").click(function(){
+      $("div.county_info").hide();
+      $("div.chippewa").show();
+   });
+   
+   $("area.iosco").click(function(){
+      $("div.county_info").hide();
+      $("div.clare").show();
    });
 }
-
-      <area name="benzie" shape="poly" coords="" href="#" alt="Benzie">
-      <area name="berrien" shape="poly" coords="" href="#" alt="Berrien">
-      <area name="branch" shape="poly" coords="" href="#" alt="Branch">
-      <area name="calhoun" shape="poly" coords="" href="#" alt="Calhoun">
-      <area name="cass" shape="poly" coords="" href="#" alt="Cass">
-      <area name="charlevoix" shape="poly" coords="" href="#" alt="Charlevoix">
-      <area name="cheboygan" shape="poly" coords="" href="#" alt="Cheboygan">
-      <area name="chippewa" shape="poly" coords="" href="#" alt="Chippewa">
-      <area name="clare" shape="poly" coords="" href="#" alt="Clare">
-      <area name="clinton" shape="poly" coords="" href="#" alt="Clinton">
-      <area name="crawford" shape="poly" coords="" href="#" alt="Crawford">
-      <area name="delta" shape="poly" coords="" href="#" alt="Delta">
-      <area name="dickinson" shape="poly" coords="" href="#" alt="Dickinson">
-      <area name="eaton" shape="poly" coords="" href="#" alt="Eaton">
-      <area name="emmet" shape="poly" coords="" href="#" alt="Emmet">
-      <area name="genesee" shape="poly" coords="" href="#" alt="Genesee">
-      <area name="gladwin" shape="poly" coords="" href="#" alt="Gladwin">
-      <area name="gogebic" shape="poly" coords="" href="#" alt="Gogebic">
-      <area name="grand_traverse" shape="poly" coords="" href="#" alt="Grand Traverse">
-      <area name="gratiot" shape="poly" coords="" href="#" alt="Gratiot">
-      <area name="hillsdale" shape="poly" coords="" href="#" alt="Hillsdale">
-      <area name="houghton" shape="poly" coords="" href="#" alt="Houghton">
-      <area name="huron" shape="poly" coords="" href="#" alt="Huron">
-      <area name="ingham" shape="poly" coords="" href="#" alt="Ingham">
-      <area name="ionia" shape="poly" coords="" href="#" alt="Ionia">
-      <area name="iosco" shape="poly" coords="" href="#" alt="Iosco">
+      
       <area name="iron" shape="poly" coords="" href="#" alt="Iron">
       <area name="isabella" shape="poly" coords="" href="#" alt="Isabella">
       <area name="jackson" shape="poly" coords="" href="#" alt="Jackson">
